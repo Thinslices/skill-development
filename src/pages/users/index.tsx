a886b656c46d2fb0105eb3be561b3626147dcdb8
Razvan Onofrei <razwan.me@gmail.com>
2023-02-13 22:08:13 +0200

    Add breadcrumbs and layout to users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,23 +2,25 @@ import type { User } from "@prisma/client";
 import { type NextPage } from "next";
 import { useTable } from "react-table";
 
-import { Header, Table, Wrapper } from "../../components";
+import { Breadcrumbs, Layout, Table } from "../../components";
 import { useUserColumns } from "../../components/UserTable/useUserColumns";
+import { useBreadcrumbs } from "../../hooks";
 import { api } from "../../utils/api";
 
 const UsersPage: NextPage = () => {
   const columns = useUserColumns();
   const { data } = api.user.getAll.useQuery();
   const tableInstance = useTable<User>( { columns, data: data ?? [] } )
+  const breadcrumbs = useBreadcrumbs();
 
   return (
-    <>
-      <Header />
-      <Wrapper className="py-14">
+    <Layout>
+      <div className="space-y-8">
+        <Breadcrumbs breadcrumbs={ breadcrumbs } />
         <h1 className="h1 mb-12">Users</h1>
         <Table tableInstance={ tableInstance } />
-      </Wrapper>
-    </>
+      </div>
+    </Layout>
   );
 };
 
