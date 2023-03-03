04c8140921012ce2ea2d7ec889bb223eafa22c80
Diana Toma <diana.toma@thinslices.com>
2023-03-03 15:52:27 +0200

    test branch

--- src/components/QuestionListEdit/QuestionListEdit.tsx
+++ src/components/QuestionListEdit/QuestionListEdit.tsx
@@ -1,26 +1,34 @@
-import { QuestionForm } from '..';
-import type { Question } from '../../utils/types';
+import { QuestionForm } from "..";
+import type { Question } from "../../utils/types";
 
 type QuestionListEditProps = {
-    questions: Question[],
-    onQuestionChange: ( index: number, newQuestion: Question ) => void
-    addQuestion: ( index: number ) => void,
-}
+    questions: Question[];
+    onQuestionChange: (index: number, newQuestion: Question) => void;
+    addQuestion: (index: number) => void;
+};
 
-export const QuestionListEdit:React.FC<QuestionListEditProps> = ( props ) => {
-    const { addQuestion, onQuestionChange, questions } = props;
-
+export const QuestionListEdit = ({
+    addQuestion,
+    onQuestionChange,
+    questions,
+}: QuestionListEditProps) => {
     return (
         <>
-            { questions.map( ( question: Question, index: number ) => {
+            {questions.map((question: Question, index: number) => {
                 return (
-                    <QuestionForm key={ index } index={ index } data={ question } onAnswerEnterKeyDown={ () => {
-                        addQuestion( index + 1 );
-                    } } onChange={ ( newQuestion ) => {
-                        onQuestionChange( index, newQuestion );
-                    } } />
-                )
-            } ) }
+                    <QuestionForm
+                        key={index}
+                        index={index}
+                        data={question}
+                        onAnswerEnterKeyDown={() => {
+                            addQuestion(index + 1);
+                        }}
+                        onChange={newQuestion => {
+                            onQuestionChange(index, newQuestion);
+                        }}
+                    />
+                );
+            })}
         </>
-    )
-}
+    );
+};
